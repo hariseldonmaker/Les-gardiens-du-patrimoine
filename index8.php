8d9b407da97d326118d1bdad30b73de608d92d0b
hariseldonmaker <81969762+hariseldonmaker@users.noreply.github.com>
2021-04-29 11:37:31 +0200

    Create index8.php

---
+++ index8.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <meta charset="utf-8"/>
+  <meta charset="viewport" content="width=device-width">
+  <meta type="description" content="Events : Next coming event/ Historical pageant/ Battle of Imryrr/ Castle of Imryrr">
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" media="screen and (max-width: 700px)" href="style.css" type="text/css"/>
+    <nav>
+      <p class="nav-item-active"></p> <a href="file:///C:/Users/julie/Videos/Les%20gardiens%20du%20patrimoine%20Accueil%20English.html"><b>Homepage</b></a> 
+      <p class="nav-item-active"></p> <a href="Evènements"><b>Events</b></a> 
+      <p class="nav-item-active"></p> <a href="file:///C:/Users/julie/Videos/Les%20gardiens%20du%20patrimoine%20A%20propos%20English.html"><b>About</b></a>
+      <p class="nav-item-active"></p> <a href="file:///C:/Users/julie/Videos/Les%20gardiens%20du%20patrimoine%20Contact%20English.html"><b>Contact</b></a>
+ </nav>
+    <title>Events</title>
+  </head>
+  <body> 
+   <main>
+      <h1>
+     Events 
+      </h1>
+      <h2>
+        Coming event : 
+        <i>Battle pageant at the castle of Imryrr</i>
+      </h2>
+      <p><a href="file:///C:/Users/julie/Videos/Les%20gardiens%20du%20patrimoine%20Ev%C3%A8nements.html"><b>Français</b></a>&emsp;&emsp;&emsp;&emsp;<b>English</b></p>
+       <p>
+      </p>
+       <p> 
+      </p>
+      <a>
+        <img src="https://www.isere-tourisme.com/sites/default/files/styles/image_w100_ito3/public/sitra/161235_812894.jpg" alt="Une image cliquable">
+      </a>
+      <p>
+      </p>
+      <p>
+      </p>
+      <p>
+      <p>The castle of Imryrr, nicknamed "The dreaming city" is an architectural jewel. Unfortunately, as becoming old, its roofs have been damaged. The funds gained during this great day will benefit to the roofing repair.</p>
+      <p>The castle of Imryrr has been built on the plain of Melnibone in 1612, and sees itself as a model of architectural audaciousness, while keeping typical caracteristics of this era. It is therefore one of the most preserved in the department of Isère (38). In the past, its exceptional frame has offered us some of our most beautiful demonstrations. Today, it's the castle itself which needs a little helping hand.</p> 
+      <p>Our show wil give rebirth to the great battle of 1642, when the Savoie duchy troops had taken assault against the fortress, strategic place of the France kingdom in the Alps. The grand premiere will take place on the <b>Sunday 23th of May 2021</b> from <b>14:30 to 16:00</b>. To recover from our emotions, then there will be a cocktail reception, and it will be possible to discuss with the "knights" who will have participated in the show.</p>
+      <box>
+      <span><p><b>Warning ! Be on time, because the castle access will be reserved to the only spectators, and the latecomers won't be accepted !</b></p></span> 
+    <p></p>
+    </box>
+    </p>
+      <p>
+      </p>
+    </main>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <footer> 
+      All rights reserved, April 2021 ©
+      <p>
+      </p>
+      Les gardiens du patrimoine 
+      <p>
+      </p> 
+      Association loi 1901, based in Lyon (Rhône) 
+      <p>
+      </p>     
+    </footer>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    </body> 
+    <style>
+      body {
+        font-family: lato; 
+        margin: 1rem;
+        background: lightgoldenrodyellow;       
+      }
+      box {
+        display : center; 
+        position : block; 
+        width: 500px;
+        height: 700;
+        background: rgb(255, 255, 255);
+      }
+      span {
+        margin-left: 1rem;
+        flex-direction: row;
+        align-items: center;
+        text-shadow: black;
+        color: rgb(236, 13, 13);
+        text-size-adjust: 12rem;
+      }
+      nav {
+        margin-bottom: 2rem;
+        background: rgb(255, 235, 179);
+      }
+      footer{ 
+        margin-top: 2rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        flex-direction: column;
+        align-items: center;
+        color: black;
+        background: rgb(203, 216, 230);
+      }
+      nav, footer{
+        display: flex;
+        justify-content: space-around;
+        height: 100px;
+        -webkit-box-shadow: 0px 0px 40px 0px rgba(186,186,186,1); 
+        -moz-box-shadow: 0px 0px 40px 0px rgba(186,186,186,1);
+        box-shadow: 0px 0px 40px 0px rgba(186,186,186,1);
+      }
+      main { 
+        display: flex; 
+        flex-direction: column;
+        align-items: center;
+      }
+      h1, h2 {
+        text-align: center;
+      }
+      .nav-item {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        color: black;      
+      }
+    </style>
+</html>
